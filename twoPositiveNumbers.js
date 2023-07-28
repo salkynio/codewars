a624033d306228e5bf576ce504ba494b0c475126
salkyn <omursalkyn@gmail.com>
2023-07-28 22:42:32 +0600

    -add description

--- twoPositiveNumbers.js
+++ twoPositiveNumbers.js
@@ -1,3 +1,7 @@
+//description
+// Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+
+
 //first solution
 function twoArePositive(a, b, c) {
   let count = 0;
